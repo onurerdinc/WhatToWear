2bc2f0d0d98fbebe14003d9dc64d6440a9d33a89
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-09-12 09:42:54 +0300

    add header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="header logo" />
+      <p className="header__date-and-location">Date, Location</p>
+      <button className="header__add-close-button">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="avatar name" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">Today is 75° F / You may want to wear:</p>
+        {/* add the cards */}
+      </section>
+    </main>
+  );
+}
+
+export default Main;
