5ee112c15d46dd73d47a0eeea7fece1b31a329c4
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-10-02 14:30:50 -0400

    fix ItemModal.css

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -10,8 +10,10 @@ function ItemModal({ activeModal, onClose, card, confirmationModal }) {
         </button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <div className="modal__caption-weather-container">
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
           <button className="modal__delete" onClick={confirmationModal}>
             Delete item
           </button>
