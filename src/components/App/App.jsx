571a8ef5ddf2f1c727f168cd601daaf29e2336a7
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-10-02 11:57:00 -0400

    add route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { coordinates } from "../../utils/constants";
 import { APIkey } from "../../utils/constants";
@@ -65,7 +66,19 @@ function App() {
       >
         <div className="page__content">
           <Header handleAddClick={handleAddClick} weatherData={weatherData} />
-          <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Main
+                  weatherData={weatherData}
+                  handleCardClick={handleCardClick}
+                />
+              }
+            />
+            <Route path="/profile" element={<p>Profile</p>} />
+          </Routes>
+
           <Footer />
         </div>
         <AddItemModal
