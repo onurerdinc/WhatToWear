ece148167144ae27a7b5048a27546e2018acca6f
Onur Erdinc <onurerdnc@gmail.com>
2024-11-13 16:25:50 -0500

    .

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,7 +1,9 @@
-import { React, useState } from "react";
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import React, { useState } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
+import "../ModalWithForm/ModalWithForm.css";
+import "./AddItemModal.css";
 
-const AddItemModal = ({ onClose, onAddItem, isOpen, isLoading }) => {
+const AddItemModal = ({ closeActiveModal, addItem, isOpen }) => {
   const [name, setName] = useState("");
   const [imageUrl, setUrl] = useState("");
   const [weather, setSelectedWeatherType] = useState("");
@@ -22,7 +24,7 @@ const AddItemModal = ({ onClose, onAddItem, isOpen, isLoading }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAddItem({ name, imageUrl, weather }, resetForm);
+    addItem({ name, imageUrl, weather }, resetForm);
   };
 
   function resetForm() {
@@ -30,71 +32,84 @@ const AddItemModal = ({ onClose, onAddItem, isOpen, isLoading }) => {
     setUrl("");
     setSelectedWeatherType("");
   }
+
   return (
     <ModalWithForm
       title="New garment"
-      buttonText={isLoading ? "Saving..." : "Add garment"}
+      onClose={closeActiveModal}
       isOpen={isOpen}
-      onClose={onClose}
       onSubmit={handleSubmit}
     >
       <label htmlFor="name" className="modal__label">
-        Name
+        Name{" "}
         <input
           type="text"
           className="modal__input"
           id="name"
           placeholder="Name"
-          onChange={handleNameChange}
           value={name}
-          required
+          onChange={handleNameChange}
+          minLength="1"
+          maxLength="30"
         />
       </label>
       <label htmlFor="imageUrl" className="modal__label">
-        Image
+        Image{" "}
         <input
           type="url"
           className="modal__input"
           id="imageUrl"
           placeholder="Image URL"
-          onChange={handleUrlChange}
           value={imageUrl}
-          required
+          minLength="1"
+          onChange={handleUrlChange}
         />
       </label>
       <fieldset className="modal__radio-buttons">
         <legend className="modal__legend">Select the weather type:</legend>
-        <label className="modal__label modal__label_type_radio">
+        <label
+          htmlFor="hot"
+          className={`modal__label modal__label_type_radio ${
+            weather === "hot" ? "modal__label_type_radio--selected" : ""
+          }`}
+        >
           <input
+            id="hot"
             type="radio"
             className="modal__radio-input"
-            name="option"
-            id="hot"
             value="hot"
             checked={weather === "hot"}
             onChange={handleWeatherChange}
           />
           Hot
         </label>
-        <label className="modal__label modal__label_type_radio">
+        <label
+          htmlFor="warm"
+          className={`modal__label modal__label_type_radio ${
+            weather === "warm" ? "modal__label_type_radio--selected" : ""
+          }`}
+        >
           <input
+            id="warm"
             type="radio"
             className="modal__radio-input"
-            id="warm"
             value="warm"
-            name="option"
             checked={weather === "warm"}
             onChange={handleWeatherChange}
           />
           Warm
         </label>
-        <label className="modal__label modal__label_type_radio">
+        <label
+          htmlFor="cold"
+          className={`modal__label modal__label_type_radio ${
+            weather === "cold" ? "modal__label_type_radio--selected" : ""
+          }`}
+        >
           <input
+            id="cold"
             type="radio"
             className="modal__radio-input"
-            id="cold"
             value="cold"
-            name="option"
             checked={weather === "cold"}
             onChange={handleWeatherChange}
           />
